5f722f4bd6ff8ebe059f5dbf879883cf30b5998b
LocoNeko <loconeko73@gmail.com>
2013-11-14 12:36:36 +0900

    Initiative bidding

--- src/ROR/Party.php
+++ src/ROR/Party.php
@@ -12,9 +12,9 @@ class Party
      * $leader (Senator) : The Senator who is the party's leader (must be in the $senators Deck) or NULL
      * $treasury (int)  : Current Party's treasury
      * $phase_done (bool) : Whether this party has played its phase or not
-     * $initiativeBid (int) : The party's current initiative bid or NULL
-     * $initiativeBidDone (bool) : whether or not this party is done bidding for initiative or NULL
-     * $initiativeBidWith (Senator) : the senator the party is bidding with or NULL
+     * $bid (int) : The party's current initiative/persuasion bid
+     * $bidDone (bool) : whether or not this party is done bidding for initiative or NULL (WARNING : This is NOT used for persuasion)
+     * $bidWith (Senator) : the senator the party is bidding with for initiative/persuasion or NULL
      */
     public $user_id;
     public $name ;
@@ -24,7 +24,7 @@ class Party
     public $leader ;
     public $treasury ;
     public $phase_done ;
-    public $initiativeBid , $initiativeBidDone , $initiativeBidWith  ;
+    public $bid , $bidDone , $bidWith  ;
     
     public function __construct() {
     }
@@ -40,9 +40,9 @@ class Party
         $this->leader = NULL ;
         $this->treasury = 0 ;
         $this->phase_done = FALSE ;
-        $this->initiativeBid = NULL ;
-        $this->initiativeBidDone = NULL ;
-        $this->initiativeBidWith = NULL ;
+        $this->bid = 0 ;
+        $this->bidDone = NULL ;
+        $this->bidWith = NULL ;
     }
     
     public function fullName() {
